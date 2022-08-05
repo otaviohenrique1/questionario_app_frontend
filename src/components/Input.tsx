028381f115bef494fbd069f3e5a8d77c7d4d9989
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-08-05 18:06:23 -0300

    commit 6

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
-import { ErrorMessage, Field } from 'formik'
+import { ErrorMessage, Field/* , FieldArray */ } from 'formik'
 import React, { HTMLAttributes, InputHTMLAttributes, ReactNode } from 'react'
-import { Label, LabelProps } from 'reactstrap';
+import { /* Col, */ Label, LabelProps/* , Row */ } from 'reactstrap';
 import styled from 'styled-components';
 
 export type InputProps = InputHTMLAttributes<HTMLInputElement>;
@@ -11,9 +11,11 @@ export function Input(props: InputProps) {
   )
 }
 
-export type FormInputProps = InputProps & {
+export type PaddingMarginStyleProps = {
   containerPaddingMarginProps?: FormInputContainerStyledProps;
 };
+
+export type FormInputProps = InputProps & PaddingMarginStyleProps;
 
 export function FormInput(props: FormInputProps) {
   return (
@@ -61,12 +63,45 @@ const FormInputContainerStyled = styled.div<FormInputContainerProps>`
   margin-right: ${(props) => props.margin_right || "0"};
 `;
 
-export type CheckboxProps = {
+export type CheckboxProps = PaddingMarginStyleProps & {
   label_texto: ReactNode;
   label_props: LabelProps;
   input_props: InputProps;
 };
 
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <FormInputContainer
+      className="d-flex flex-column"
+      {...props.containerPaddingMarginProps}
+    >
+      <Input {...props.input_props} type="checkbox" />
+      <Label {...props.label_props}>{props.label_texto}</Label>
+    </FormInputContainer>
+  )
+}
+
+export type RadioProps = PaddingMarginStyleProps & {
+  label_texto: ReactNode;
+  label_props: LabelProps;
+  input_props: InputProps;
+};
+
+export function Radio(props: RadioProps) {
+  return (
+    <FormInputContainer
+      className="d-flex flex-column"
+      {...props.containerPaddingMarginProps}
+    >
+      <Input {...props.input_props} type="radio" />
+      <Label {...props.label_props} htmlFor={props.input_props.id}>
+        {props.label_texto}
+      </Label>
+    </FormInputContainer>
+  )
+}
+
+/*
 export function Checkbox(props: CheckboxProps) {
   return (
     <Label {...props.label_props}>
@@ -76,17 +111,55 @@ export function Checkbox(props: CheckboxProps) {
   )
 }
 
-export type RadioProps = {
-  label_texto: ReactNode;
-  label_props: LabelProps;
-  input_props: InputProps;
-};
-
 export function Radio(props: RadioProps) {
   return (
-    <Label {...props.label_props}>
+    <Label {...props.label_props} htmlFor={props.input_props.id}>
       <Input {...props.input_props} type="radio" />
       {props.label_texto}
     </Label>
   )
 }
+*/
+
+/* export type InputArrayProps = { 
+  data: [],
+  label_texto: ReactNode;
+  label_props: LabelProps;
+  input_props: InputProps;
+}
+
+export function InputArray(props: InputArrayProps) {
+  return (
+    <FieldArray name="friends">
+      {({ insert, remove, push }) => (
+        <div>
+          {values.friends.length > 0 &&
+            values.friends.map((friend, index) => (
+              <Row className="row" key={index}>
+                <Col>
+                  <Label htmlFor={`friends.${index}.name`}>Name</Label>
+                  <Input
+                    name={`friends.${index}.name`}
+                    placeholder="Jane Doe"
+                    type="text"
+                  />
+                  <ErrorMessage
+                    name={`friends.${index}.name`}
+                    component="span"
+                    className="alert "
+                  />
+                </Col>
+              </Row>
+            ))}
+          <button
+            type="button"
+            className="secondary"
+            onClick={() => push({ name: '', email: '' })}
+          >
+            Add Friend
+          </button>
+        </div>
+      )}
+    </FieldArray>
+  );
+} */
